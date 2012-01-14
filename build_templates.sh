c7e95966861d9f98b11ac1db78e874fa60d6c36a
Thomas Levine <thomas.levine@gmail.com>
2012-01-14 16:48:06 -0500

    templates

---
+++ build_templates.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+for file in templating/*.html; do
+  basename="`basename \"$file\" '.html'`"
+  if [[ $basename != base ]]; then
+    sed "/<!--Page-specific content below-->/r $file" templating/base.html > $basename.html
+  fi
+done

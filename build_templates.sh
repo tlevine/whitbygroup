dfe38d24a9ce70fe58bdc4dda79dff1d40ebd54b
Thomas Levine <thomas.levine@gmail.com>
2012-01-14 16:55:04 -0500

    build script yay

--- build_templates.sh
+++ build_templates.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+#Put all files in templating/ other than base.html
+#into the #main div in the base.html file.
+
 for file in templating/*.html; do
   basename="`basename \"$file\" '.html'`"
-  if [ "$basename" = base ]; then
+  if [ "$basename" != base ]; then
     sed "/<!--Page-specific content below-->/r $file" templating/base.html > $basename.html
   fi
 done

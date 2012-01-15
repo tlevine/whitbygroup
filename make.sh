501827b27f38ee90db5dd762e5ff09389680ee1b
Thomas Levine <thomas.levine@gmail.com>
2012-01-14 19:26:39 -0500

    templates dir

--- make.sh
+++ make.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 domainname=whitbygroup.com
 
-#Ant build script
-cd build
-ant
-cd -
-
 #Put all files in templating/ 
 #into the #main div in the published html file.
 
-for file in publish/*.html; do
+for file in templates/*.html; do
   sed "/<div id=main role=main>/r $file" publish/index.html > publish/$basename.html
   sed -i "s/{{domainname}}/$domainname/g" publish/*.html
 done
